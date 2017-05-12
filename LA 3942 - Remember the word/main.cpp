3417bea002a163b5ad5e5542168ff56db247dca0
gaole <gaole@umich.edu>
2017-05-11 23:03:43 -0400

    revise

--- LA 3942 - Remember the word/main.cpp
+++ LA 3942 - Remember the word/main.cpp
@@ -1,95 +1,104 @@
-#include <iostream>
-#include <cstring>
-#include <cstdio>
-#include <string>
+#include "algorithm"
+#include "iostream"
+#include "cstring"
+#include "cstdio"
+#include "string"
+#include "stack"
+#include "cmath"
+#include "queue"
+#include "set"
+#include "map"
+ 
+#define lson l , m , rt << 1
+#define rson m + 1 , r , rt << 1 | 1
+ 
+typedef long long ll;
+typedef unsigned long long ull;
 using namespace std;
-
-
-
-
-const int sigma_size = 26;
-const int maxnode = 4000;
-long long int d[300000+10];
-
-struct Trie {
-	int ch[maxnode][sigma_size];
-	int val[maxnode];
-	int sz;
-	Trie() {
-		sz  = 1;
-		memset(ch[0], 0, sizeof(ch[0]));
-	}
-	int idx(char c){
-		return c-'a';
-	}
-	void insert(string& s, int v){
-		int u = 0, len = s.length();
-		for (int i = 0; i < len; i++){
-			int c = idx(s[i]);
-			if (!ch[u][c]){
-				memset(ch[sz], 0, sizeof(ch[sz]));
-				val[sz] = 0;
-				ch[u][c] = sz++;
-			}
-			u = ch[u][c];
-		}
-		val[u] = v;
-	}
-	void lookup(const string& s, int p){
-		int u = 0, len = s.length();
-		for (int i = p; i < len; i++){
-			int c = idx(s[i]);
-			if (!ch[u][c]){
-				break;
-			}
-			u = ch[u][c];
-			if (val[u]){
-				if (i+1 >= s.length()){
-					d[p] += (long long int)1 ;
-					d[p] %= 20071027;
-				}
-				else{
-					d[p] += d[1+i] ;
-					d[p] %= 20071027;
-				}
-			}
-		}
-	}
-
-
-};
-
-
-
-
-
-
- int main(){
-
- 	
- 	string str;
- 	int kase = 1;
- 	while (cin >> str){
- 		int S;
- 		cin >> S;
- 		memset(d, 0, sizeof(d));
- 		Trie dic;
-
- 		for (int i = 0; i < S; i++){
- 			string tmp;
- 			cin >> tmp;
- 			dic.insert(tmp, 1);
- 		}
- 		int slen = str.length();
- 		for (int i = slen-1; i>=0; i--){
- 			dic.lookup(str, i);
- 		}
- 		// for (int i =0; i< str.length(); i++){
- 		// 	cout << d[i] <<" ";
- 		// }
- 		// cout << endl;
- 		cout << "Case "<<kase++<<": "<< d[0]<<"\n"; 
- 	}
-
- 	return 0;
- }
+ 
+const int inf=0x3f3f3f3f;
+const int maxn=1e6+5;
+const int mod=20071027;
+ 
+char str[maxn];
+char word[maxn];
+int dp[maxn];
+int n;
+ 
+ 
+ 
+struct Trie
+{
+    int sz;
+    int ch[maxn][26];
+    int val[maxn];
+ 
+ 
+    void Init()
+    {
+        sz=1;
+        val[0]=0;
+        memset(ch[0],0,sizeof ch[0]);
+ 
+    }
+ 
+    void Insert(char *s)
+    {
+        int n = strlen(s);
+        int u = 0;
+        for(int i=0;i<n;++i)
+        {
+            if( ch[u][ s[i]-'a' ] ==0 )
+            {
+                memset(ch[sz],0,sizeof ch[sz]);//!!
+                val[sz]=0;
+                ch[u][ s[i]-'a' ] = sz;
+                sz++;
+            }
+            u = ch[u][ s[i]-'a' ];
+        }
+        val[u]=n;
+        //printf("insert:%s\n",s);
+    }
+ 
+    void Search(char * s,int k,int l)
+    {
+        int u = 0;
+        for(int i=0;i<l;++i)
+        {
+            if( ch[u][ s[i]-'a' ]==0 )return;
+            u = ch[u][ s[i]-'a' ];
+            if( val[u] )
+            {
+                dp[k] = ( dp[k] + dp[k+val[u] ] )%mod;
+            }
+        }
+    }
+}trie;
+ 
+int main()
+{
+    //freopen("in.txt","r",stdin);
+    int cs=1;
+    while( scanf("%s",&str)!=EOF )
+    {
+        trie.Init();
+ 
+        scanf("%d",&n);
+        for(int i=0;i<n;++i)
+        {
+            scanf("%s",&word);
+            trie.Insert(word);
+        }
+        int len = strlen(str);
+        dp[len]=1;
+        for(int i=len-1;i>=0;--i)
+        {
+            dp[i]=0;
+            trie.Search(str+i,i,len-i);
+        }
+        printf("Case %d: %d\n",cs++,dp[0]);
+    }
+ 
+    return 0;
+}
